d896ab57197c2111801f13127b9fb919453dd03b
Giuliano Serawop <gserawop13@gmail.com>
2021-04-26 17:27:16 -0600

    polishing up html and node questions

--- index.js
+++ index.js
@@ -199,7 +199,27 @@ function teamQuestions (){
 
 function createHtml(){
   // console.log(employeesArray)
-  let htmlString ;
+  let htmlString = `
+    <!doctype html>
+<html lang="en"> n
+  <head>
+    <title>Team Profile</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body class="text-center">
+    <header>
+      <h1 class="p-5">
+        <u>The A Team</u>
+      </h1>
+    </header>
+      <div class="container p-4">
+  `; ;
   for(i = 0; i < employeesArray.length; i++){
     if (employeesArray[i].github){
       htmlString += `
@@ -207,6 +227,11 @@ function createHtml(){
         <p> ${employeesArray[i].id} </p>
         <p> ${employeesArray[i].email} </p>
         <p> ${employeesArray[i].github} </p>
+        </div>
+      <footer>
+        <h3 class="p-4">&copy; Giuliano Serawop | 2021</h3>
+      </footer>
+    </div>
 `;
   }
     else if (employeesArray[i].officeNumber){
@@ -214,23 +239,40 @@ function createHtml(){
         <p> ${employeesArray[i].name} </p>
         <p> ${employeesArray[i].id} </p>
         <p> ${employeesArray[i].email} </p>
-        <p> ${employeesArray[i].github} </p>
+        <p> ${employeesArray[i].officeNumber} </p>
+        </div>
+      <footer>
+        <h3 class="p-4">&copy; Giuliano Serawop | 2021</h3>
+      </footer>
+    </div>
 `;
-    }  else if (employeesArray[i].officeNumber){
+    }  else if (employeesArray[i].school){
        htmlString += `
         <p> ${employeesArray[i].name} </p>
         <p> ${employeesArray[i].id} </p>
         <p> ${employeesArray[i].email} </p>
-        <p> ${employeesArray[i].github} </p>
+        <p> ${employeesArray[i].school} </p>
+        </div>
+      <footer>
+        <h3 class="p-4">&copy; Giuliano Serawop | 2021</h3>
+      </footer>
+    </div>
+
 `;
-    }
-      fs.writeFile("index2.html", htmlString, (err) => {
-    if (err) throw err;
+  }
+}
+htmlString += ` 
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
+`;
+    fs.writeFile("index2.html", htmlString, (err) => {
+      if (err) throw err;
+      
     console.log("The file has been saved!");
-   })     
+   });
 }
 
-
-teamQuestions()
-// Generate html file with user input
-
+teamQuestions();
