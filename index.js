6aca92060bec24f62f5f2532594908223a28abae
Giuliano Serawop <gserawop13@gmail.com>
2021-05-04 15:19:37 -0600

    updated html

--- index.js
+++ index.js
@@ -201,7 +201,7 @@ function createHtml(){
   // console.log(employeesArray)
   let htmlString = `
     <!doctype html>
-<html lang="en"> n
+<html lang="en">
   <head>
     <title>Team Profile</title>
     <!-- Required meta tags -->
@@ -219,49 +219,47 @@ function createHtml(){
       </h1>
     </header>
       <div class="container p-4">
+        
   `; ;
   for(i = 0; i < employeesArray.length; i++){
     if (employeesArray[i].github){
       htmlString += `
+      <div class="card" id="card">
         <p> ${employeesArray[i].name} </p>
         <p> ${employeesArray[i].id} </p>
         <p> ${employeesArray[i].email} </p>
         <p> ${employeesArray[i].github} </p>
-        </div>
-      <footer>
-        <h3 class="p-4">&copy; Giuliano Serawop | 2021</h3>
-      </footer>
-    </div>
+      </div>
+      
 `;
   }
     else if (employeesArray[i].officeNumber){
        htmlString += `
+       <div class="card" id="card">
         <p> ${employeesArray[i].name} </p>
         <p> ${employeesArray[i].id} </p>
         <p> ${employeesArray[i].email} </p>
         <p> ${employeesArray[i].officeNumber} </p>
         </div>
-      <footer>
-        <h3 class="p-4">&copy; Giuliano Serawop | 2021</h3>
-      </footer>
-    </div>
+      
 `;
     }  else if (employeesArray[i].school){
        htmlString += `
+       <div class="card" id="card>
         <p> ${employeesArray[i].name} </p>
         <p> ${employeesArray[i].id} </p>
         <p> ${employeesArray[i].email} </p>
         <p> ${employeesArray[i].school} </p>
         </div>
-      <footer>
-        <h3 class="p-4">&copy; Giuliano Serawop | 2021</h3>
-      </footer>
-    </div>
 
 `;
   }
 }
 htmlString += ` 
+</div>
+<footer>
+  <h3 class="p-4">&copy; Giuliano Serawop | 2021</h3>
+</footer>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
